ff88bfc934f26ba9b21483c862dde7a7ce324ff8
Monicacholico <monicachol@hotmail.com>
2019-11-10 12:31:07 -0600

    finished 2nd quizz of 2nd section

--- basics-01-starting/assets/scripts/app.js
+++ basics-01-starting/assets/scripts/app.js
@@ -1,1 +1,9 @@
-alert("I'm working");
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = currentResult + 10;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
+
+outputResult(currentResult, calculationDescription);
+
