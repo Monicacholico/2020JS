ac8ae82b9cef05c2b08dcdec7c47f6454d1a2b95
Monicacholico <monicachol@hotmail.com>
2019-11-11 22:15:02 -0600

    first exercise

--- basics-01-starting/assets/scripts/app.js
+++ basics-01-starting/assets/scripts/app.js
@@ -1,9 +1,26 @@
-const defaultResult = 0;
-let currentResult = defaultResult;
+// const defaultResult = 0;
+// let currentResult = defaultResult;
 
-currentResult = currentResult + 10;
+// currentResult = currentResult + 10;
 
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
+// let calculationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
 
-outputResult(currentResult, calculationDescription);
+// outputResult(currentResult, calculationDescription);
 
+// let userInput = 20; 
+// let result = 0;
+
+// result = 18 * userInput;
+
+// result = userInput - 10;
+// result = result * 2;
+// result = result / 4;
+
+// alert(userInput);
+// alert(result);
+
+
+// let resultDescription = (result - 15) * 2 / 3
+
+// alert(`This is the result: ${result}`);
+// alert(`This is the User Input ${userInput}`);
