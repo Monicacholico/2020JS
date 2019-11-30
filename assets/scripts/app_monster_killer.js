34beb01460139ec93f57b32ffffb02dd7435d838
Monicacholico <monicachol@hotmail.com>
2019-11-30 17:58:27 -0600

    adding if statements to function" ;

--- assets/scripts/app_monster_killer.js
+++ assets/scripts/app_monster_killer.js
@@ -1,4 +1,6 @@
 const attackValue = 10;
+const MonsterAttackValue = 14;
+
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
@@ -8,8 +10,12 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler(){
     const damage = dealMonsterDamage(attackValue);
     currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MonsterAttackValue);
+    currentPlayerHealth -= playerDamage;
     if(currentMonsterHealth <= 0){
         alert('You won');
+    } else if (currentPlayerHealth <=0){
+        alert('You lost');
     }
 }
 
