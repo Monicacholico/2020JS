229ed7bc0d5cc4aa1f500460967a7b71e11aaaa5
Monicacholico <monicachol@hotmail.com>
2019-12-01 23:02:05 -0600

    refactoring to les DRY

--- assets/scripts/app_monster_killer.js
+++ assets/scripts/app_monster_killer.js
@@ -1,5 +1,6 @@
 const attackValue = 10;
-const MonsterAttackValue = 14;
+const monsterAttackValue = 14;
+const strongAtttackValue = 17;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -7,16 +8,33 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler(){
-    const damage = dealMonsterDamage(attackValue);
+function attackMonster(mode){
+    let maxDamage;
+    if(mode === 'ATTACK'){
+        maxDamage = attackValue;
+    } else if (mode === 'STRONG_ATTACK') {
+        maxDamage = strongAtttackValue;
+    }
+    const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
-    const playerDamage = dealPlayerDamage(MonsterAttackValue);
+    const playerDamage = dealPlayerDamage(monsterAttackValue);
     currentPlayerHealth -= playerDamage;
-    if(currentMonsterHealth <= 0){
+    if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
         alert('You won');
-    } else if (currentPlayerHealth <=0){
+    } else if (currentPlayerHealth <=0 && currentMonsterHealth > 0){
         alert('You lost');
+    } else if (currentPlayerHealth <=0 && currentMonsterHealth <= 0){
+        alert('You have a draw!');
     }
 }
 
+function attackHandler(){
+    attackMonster('ATTACK');
+}
+
+function strongAttackHandler(){
+    attackMonster('STRONG_ATTACK');
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
