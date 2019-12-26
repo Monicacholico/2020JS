3a60bcc8562666fb9397657e261e174c881d5cb5
Monicacholico <monicachol@hotmail.com>
2019-12-25 23:41:13 -0600

    chapter 117

--- assets/scripts/block_scope.js
+++ assets/scripts/block_scope.js
@@ -1,25 +1,29 @@
-let name = 'Max';
+// let name = 'Max';
 
-{
-  let test = 5;
-  console.log(test);
-}
+// {
+//   let test = 5;
+//   console.log(test);
+// }
 
-console.log(test);
+// console.log(test);
 
 
-let hobbies = ['Sports', 'Cooking'];
-if (name === 'Max') {
-  console.log(hobbies);
+// let hobbies = ['Sports', 'Cooking'];
+// if (name === 'Max') {
+//   console.log(hobbies);
 
-}
+// }
 
-function greet() {
-  let age = 30;
-  let name = 'Manuel';
-  console.log(name, age, hobbies);
-}
+// function greet() {
+//   let age = 30;
+//   let name = 'Manuel';
+//   console.log(name, age, hobbies);
+// }
 
-console.log(name, hobbies);
+// console.log(name, hobbies);
 
-greet();
+// greet();
+
+var userName = 'Max';
+var userName = 'Manuel';
+console.log(userName);
