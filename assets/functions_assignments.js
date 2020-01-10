10f9f56278a02aa15657e736461f94a1a8c0d4ca
Monicacholico <monicachol@hotmail.com>
2020-01-10 13:17:27 -0600

    section 6, chapter 140

--- assets/functions_assignments.js
+++ assets/functions_assignments.js
@@ -1,5 +1,40 @@
-function sayHello(name) {
+
+const sayHello = name => {
     console.log('Hi ' + name);
   }
   
   sayHello();
+
+  const sayHellov1 = name => 'Hello ' + name; 
+
+  const default_greeting = "Hello, there!"
+  const sayHello2 = (name, greeting = default_greeting) => {
+    console.log( greeting + name);
+  }
+
+  sayHello2();
+
+  const sayHello3 = () => console.log('Hello there!');
+
+  sayHello3();
+
+
+
+  const checkInput1 = (first, ...args) => args === " " ? "I don't like empty strings" : 'Well Done';
+
+
+  function checkInput(first, ...args){
+    let hasEmptyText = false;
+    for(const arg of args) {
+      if(!arg){
+        hasEmptyText = true;
+        break;
+      }
+    }
+    if(!hasEmptyText) {
+      first();
+    }
+  }
+  checkInput(() => {
+    console.log('all not empty')}, 'Hello', 'Dear', 'hi', 'what?'
+  );
