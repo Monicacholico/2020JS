5faa389a30aed45429c2775c1247d20d1bc64dc1
Monicacholico <monicachol@hotmail.com>
2020-01-14 10:37:43 -0600

    section 7 chapter 160

--- assets/scripts/dom_01.js
+++ assets/scripts/dom_01.js
@@ -1,2 +1,9 @@
 const myClass = document.getElementById('main-title');
 myClass.setAttribute('style', 'color:blue;');
+
+const ul = document.querySelector('ul');
+ul.firstElementChild;
+
+
+const firstLi = document.querySelector('li');
+firstLi.closest('body');
--- assets/scripts/dom_assignment.js
+++ assets/scripts/dom_assignment.js
@@ -4,10 +4,17 @@ first.style.backgroundColor = 'black';
 first.style.color = 'white';
 
 
-const winTitle = document.title;
-document.title = 'Assignment - Solved';
+// const winTitle = document.title;
+// document.title = 'Assignment - Solved';
+
+const docHead = document.head;
+const docHeadTitle = docHead.querySelector('title');
+docHeadTitle.textContent = 'Assignment - Solved over';
 
 const title = document.querySelector('h1');
 // title.innerText = 'Asignment - Solved';
 const body = document.body;
 body.querySelector('h1').innerHTML = 'Assignment - Solved again';
+
+
+
