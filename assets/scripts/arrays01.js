c97adef8aa5b9ca5460ca0484fc71b4c5ee3454a
Monicacholico <monicachol@hotmail.com>
2020-01-22 17:45:13 -0600

    chapter 186

--- assets/scripts/arrays01.js
+++ assets/scripts/arrays01.js
@@ -13,7 +13,72 @@ console.log(listItems);
 const arrayListItems = Array.from(listItems);
 console.log(arrayListItems);
 
-
-
 const moreNumbers = Array.from('Hi!');
 console.log(moreNumbers);
+
+const hobbies = ['cooking', 'sports'];
+
+
+const analyticsData = [[1, 1.6], [-5.4, 2.1] ];
+const renderHtml = () => {
+    let html = '';
+    for (const data of analyticsData){
+        for(const dataPoint of data){
+            // console.log(dataPoint);
+            let li = document.createElement('li');
+            li.innerHTML = dataPoint;
+            console.log(li.innerHTML);
+            let ul = document.getElementById('data-list');
+            ul.append(li);
+
+            // html = `
+            // <li>${data}</li>
+            // `;
+        }
+    }
+    // document.getElementById('data-list').innerHTML += html;
+}
+
+const myButton = document.getElementById('butt');
+myButton.addEventListener('click', renderHtml);
+
+
+const Elite = ['wide', 'giselle', 'short', 'regular', 'intermediate', 'beginner', 'low'];
+const Alpha = ['narrow', 'egyptian', 'long','strong', 'high', 'advanced', ]
+
+
+
+const usrOptions = document.querySelectorAll('select');
+console.log(usrOptions.value);
+const calculateButton = document.getElementById('calculate');
+const strength = document.getElementById('strength');
+const result = document.getElementById('results');
+console.dir(strength)
+
+
+
+function displaySelection (){
+    const userInputs = Array.from(usrOptions);
+    console.log(userInputs);
+        for(usrInput of userInputs){
+            console.log(usrInput.value);
+            const li = document.createElement('li');
+            li.innerHTML = usrInput.value;
+            result.append(li);
+        }
+
+    const usrStrengthselect = strength.value;
+    // alert(usrselect);
+    console.log(usrStrengthselect);
+    // console.log(userInputs);
+    // result.innerHTML = usrStrengthselect;
+    // result.innerHTML = usrInput.value;
+}
+
+calculateButton.addEventListener('click', displaySelection);
+
+
+
+
+
+
