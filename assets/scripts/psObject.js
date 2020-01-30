eed880b5b35c784e20a87190ff8ffd0a853ce089
Monicacholico <monicachol@hotmail.com>
2020-01-30 10:16:48 -0600

    making objects

--- assets/scripts/psObject.js
+++ assets/scripts/psObject.js
@@ -1,39 +1,47 @@
+const myPointeShoe = (function(){
 
-class psBanner {
-    constructor(pointeShoe){
-        pointeShoe.name = this.name;
-        pointeShoe.brand = this.brand;
-        pointeShoe.level = this.level;
-        pointeShoe.type = this.type;
-        pointeShoe.length = this.length;
-        pointeShoe.strength = this.strength;
-        pointeShoe.width = this.width;
-        this.build();
 
-    }
+    class psBanner {
+        constructor(pointeShoe) {
+            this.name = pointeShoe.name;
+            this.brand = pointeShoe.brand;
+            this.level = pointeShoe.level;
+            this.type = pointeShoe.type;
+            this.length = pointeShoe.length;
+            this.strength = pointeShoe.strength;
+            this.width = pointeShoe.width;
+            this.build();
+        }
+    
+        build(){
+            const html = `<div>
+            <h2>${this.name} </h2>
+            <ul class="ps-list">
+                <li> It's brand is: ${this.brand}</li>
+                <li> Best level to wear: ${this.level}</li>
+                <li> Best type of feet to wear: ${this.type}</li>
+                <li> Best length of toes to wear: ${this.length}</li>
+                <li> Best strength of feet: ${this.strength}</li>
+                <li> Recommended width to wear: ${this.width}</li>
+            </ul>
+            </div>
+            `
+            const displayer = document.getElementById('wrapper').innerHTML += html;
+            console.log(displayer);
+        }
+    };
+    
+     return psBanner;
+})();
 
-    build(){
-        const html = `<div>
-        <h2>${this.name} has: </h2>
-        <ul>
-            <li>${this.brand}</li>
-            <li>${this.level}</li>
-            <li>${this.type}</li>
-            <li>${this.length}</li>
-            <li>${this.strenght}</li>
-            <li>${this.width}</li>
-        </ul>
-        </div>
-        `
-        const displayer = document.getElementById('wrapper').innerHTML += html;
-        console.log(displayer);
-    }
-};
 
-function hello(greeting) {
-    console.log('I am Monica ' + greeting);
-};
-hello('how are you?');
+
+
+
+// function hello(greeting) {
+//     console.log('I am Monica ' + greeting);
+// };
+// hello('how are you?');
 
 
 
