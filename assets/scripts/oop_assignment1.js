a8fd262d57bb3ddccebb27e7dea709c52fd222ec
Monicacholico <monicachol@hotmail.com>
2020-03-11 17:13:22 -0500

    oop assignment

--- assets/scripts/oop_assignment1.js
+++ assets/scripts/oop_assignment1.js
@@ -1,37 +1,23 @@
-class Mourse {
+class Course {
 
     constructor(title, length, price){
         this.title = title;
         this.length = length;
         this.price = price;
-        // this.greeting();
-        this.display();
-
     }
 
-    greeting() {
-        console.log('hello');
-    }
-
-    display(){
-        new Mourse (
-            'Math', '1 hour', 15
-        )
-        console.log(new Mourse());
-        const allCourses = [];
-           const mathCourse = new Mourse(
-                'math', '5 hours', 25
-                );
+    calculateHours(){
         
-            const spanishCourse = new Mourse(
-                'spanish', '3 hours', 25
-            );
-
-        console.log(this.mathCourse.title);
-        console.log(this.spanishCourse.price);
     }
-    
+
 }
+    const mathCourse = new Course('math', '5 hours', 25);
+    
+    const spanishCourse = new Course('spanish', '3 hours', 25);
+        
+        console.log(mathCourse);
+        console.log(spanishCourse);
+
 
 function greetin(){
     console.log( 'Hey I have a javascript File working here')
