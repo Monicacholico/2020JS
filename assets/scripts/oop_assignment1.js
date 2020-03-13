888442976dbbd1d2095aabd753bc020985b302a8
Monicacholico <monicachol@hotmail.com>
2020-03-12 21:00:33 -0600

    oop assignment

--- assets/scripts/oop_assignment1.js
+++ assets/scripts/oop_assignment1.js
@@ -4,19 +4,30 @@ class Course {
         this.title = title;
         this.length = length;
         this.price = price;
+        this.calculateHours();
     }
-
-    calculateHours(){
+    
+    calculateHours(course){
+        const mathCourse = new Course('math', '5 hours', 25);
         
+        const spanishCourse = new Course('spanish', '3 hours', 25);
+            
+            console.log(mathCourse);
+            console.log(spanishCourse);
+        if (course.title === 'math'){
+            console.log(this);
+            console.log(`The Math course 
+            has a duration of ${mathCourse.lenght} 
+            and a price of ${mathCourse.price}`)
+        }
+        // if (course.title ==='spanish'){
+        //     console.log(`The Spanish course
+        //     has a duration of ${spanishcourse.lenght}
+        //     and a price of ${spanishCourse.price}`)
+        // }
     }
 
 }
-    const mathCourse = new Course('math', '5 hours', 25);
-    
-    const spanishCourse = new Course('spanish', '3 hours', 25);
-        
-        console.log(mathCourse);
-        console.log(spanishCourse);
 
 
 function greetin(){
