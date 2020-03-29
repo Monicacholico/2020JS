daa9d823785810fdc473a4c131d35fb97f5eac22
Monicacholico <monicachol@hotmail.com>
2020-03-28 23:44:33 -0500

    practicing oop

--- assets/scripts/practice-oop-1.js
+++ assets/scripts/practice-oop-1.js
@@ -3,22 +3,26 @@ class Activity {
     constructor(title, text){
         this.title = title;
         this.text = text;
+        this.build();
     }
     build(){
         const active = document.getElementById('active-projects');
         let html = '';
-        const ul = document.createElement('ul');
+        const ul = active.querySelector('ul');
+        console.log(ul);
         const li = document.createElement('li');
         li.className = 'card';
         ul.appendChild(li);
-        html = `<h2>${this.title}</h2>
+        li.innerHTML = `<h2>${this.title}</h2>
                 <p>${this.text}</p>
                 <button class="alt">More Info</button>
                 <button>Finish</button>
         `;
         console.log(ul);
         console.log(li);
-        li.appendChild(html);
+        li.append(html);
+        ul.appendChild(li);
+        // li.appendChild(html);
         // active.appendChild(ul);
         console.log(html)
     }
@@ -29,9 +33,10 @@ class Activity {
 
 const activity1 = new Activity('Shopping', 'Buy goat cheese');
 
-activity1.build();
+// activity1.build();
 console.log(activity1);
 
 // const active = document.getElementById('active-projects');
 
 const finished = document.getElementById('finished-projects');
+const finishedHtml = ``
