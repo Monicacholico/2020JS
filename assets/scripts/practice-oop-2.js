f9fe48a5ba870429112635758c3744dcfdcc9aeb
Monicacholico <monicachol@hotmail.com>
2020-04-08 23:55:17 -0500

    chapter 286

--- assets/scripts/practice-oop-2.js
+++ assets/scripts/practice-oop-2.js
@@ -10,28 +10,51 @@ class DOMHelper {
         destinationElement.append(element);
     }
 }
-class Tooltip {
+
+class Component {
+    constructor(hostElementId, insertBefore = false){
+        if(hostElementId){
+            this.hostElement = document.getElementById(hostElementId);
+        } else {
+            this.hostElement = document.body;
+        }
+        this.insertBefore = insertBefore;
+    }
+    detach() {
+        if(this.element) {
+            this.element.remove();
+            // this.element.parentElement.removeChild(this.element); for old browsers
+        }
+    }
+    show(){
+        this.hostElement.insertAdjacentElement(
+            this.insertBefore ? 'afterbegin': 'beforeend', 
+            this.element)
+    }
+}
+
+class Tooltip extends Component {
     constructor(closeNotifierFunction){
+        super('finished-projects', true);
         this.closeNotifier = closeNotifierFunction;
+        this.create();
     }
     closeTooltip = () => {
         this.detach();
         this.closeNotifier();
     }
 
-    detach() {
-        this.element.remove();
-        // this.element.parentElement.removeChild(this.element); for old browsers
-    }
-
-    show(){
+    create(){
         const tooltipElement = document.createElement('div');
         tooltipElement.className = 'card';
         tooltipElement.textContent = 'DUMMY!'
         tooltipElement.addEventListener('click', this.closeTooltip);
         this.element = tooltipElement;
-        document.body.append(tooltipElement);
     }
+
+   
+
+   
 }
 
 class ProjectItem {
