72eb99f4983ae65658218d75bff007fe51a55bcd
Monicacholico <monicachol@hotmail.com>
2020-04-10 22:36:56 -0500

    chapter 290

---
+++ assets/scripts/sizes.js
@@ -0,0 +1,11 @@
+
+// const h2 = document.getElementsByTagName('h2');
+
+// console.log(h2);
+
+// const myDiv = document.getElementsByClassName('zero');
+// console.log(myDiv);
+
+var theDiv = document.getElementById('main-box');
+console.log(theDiv);
+console.log(document.getElementById('main-box'));
