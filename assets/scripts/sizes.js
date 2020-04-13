e8639020eae6b8b81126a8e2435ab722c44f9792
Monicacholico <monicachol@hotmail.com>
2020-04-12 23:38:46 -0500

    chapter 292

--- assets/scripts/sizes.js
+++ assets/scripts/sizes.js
@@ -4,7 +4,20 @@
 // console.log(h2);
 
 const myDiv = document.getElementById('main-box');
-console.log(myDiv);
 
+console.log(myDiv); 
+console.log(myDiv.getBoundingClientRect());
+console.log(myDiv.offsetTop);
+console.log(myDiv.offsetLeft);
+console.log(myDiv.clientTop);
+console.log(myDiv.clientLeft);
+console.log(myDiv.clientWidth);
+console.log(myDiv.clientHeight);
+console.log(myDiv.scrollHeight);
+console.log(myDiv.scrollTop);
+console.log(myDiv.innerWidth);
+console.log(myDiv.innerHeight);
+console.log(document.documentElement.clientWidth);
+console.log(document.documentElement.clientHeight);
 // const thepi = document.querySelectorAll('p');
 // console.log(thepi);
