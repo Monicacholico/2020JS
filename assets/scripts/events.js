b1f1510af23b2d49fc45225f40b730a88c5ce3db
Monicacholico <monicachol@hotmail.com>
2020-04-21 23:54:53 -0500

    chapter 306

---
+++ assets/scripts/events.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('button');
+
+// button.onclick = function() {
+   
+// };
+
+// const buttonClickHandler = () => {
+//     alert('button was clicked');
+// }
+
+// button.onclick = buttonClickHandler;
+
+button.addEventListener();
+
+button.removeEventListener();
