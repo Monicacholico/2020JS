023ca6aa8d01af040e1d972569cf2ae4d212dd17
Monicacholico <monicachol@hotmail.com>
2020-04-22 00:15:34 -0500

    chapter 308

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,15 +1,34 @@
-const button = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 
 // button.onclick = function() {
    
 // };
 
-// const buttonClickHandler = () => {
-//     alert('button was clicked');
-// }
+const buttonClickHandler = (e) => {
+e.target.disabled = true
+console.log(e.target);
+} 
 
+const anotherButtonClicked = () => {
+    console.log('This was clicked');
+}
 // button.onclick = buttonClickHandler;
 
-button.addEventListener();
+// button.addEventListener('click', buttonClickHandler);
 
-button.removeEventListener();
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', buttonClickHandler);
+})
+
+// setTimeout(() => {
+//     button.removeEventListener('click',buttonClickHandler);
+// }, 2000);
+
+const boundFn = buttonClickHandler.bind(this);
+
+// button.addEventListener('click', boundFn);
+
+// setTimeout(() => {
+//     button.removeEventListener('click', boundFn);
+// }, 2000);
