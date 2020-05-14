01ac3619b5acc8b4fddf70f16059267892d6bbec
Monicacholico <monicachol@hotmail.com>
2020-05-13 23:55:32 -0500

    chapter 325

---
+++ assets/scripts/pureFunctions.js
@@ -0,0 +1,29 @@
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+console.log(add(1, 5));
+console.log(add(12, 15));
+
+function addRandom(num1) {
+    return num1 + Math.random();
+}
+console.log(addRandom(5));
+
+let previousResult = 0;
+
+function addMoreNumbers(num1, num2) {
+const sum = num1 + num2;
+previousResult = sum;
+return sum;
+}
+
+console.log(addMoreNumber(1, 5));
+
+const hobbies = ['Sports', 'Cooking'];
+function printHobbies(h) {
+    h.push('new hobby');
+    console.log(h);
+}
+
+printHobbies(hobbies);
