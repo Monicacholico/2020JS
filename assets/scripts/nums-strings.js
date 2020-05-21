de9b3e4576d5568ef4cbf4d7d63ad02b89f061fc
Monicacholico <monicachol@hotmail.com>
2020-05-20 23:56:07 -0500

    chapter 343

--- assets/scripts/nums-strings.js
+++ assets/scripts/nums-strings.js
@@ -3,3 +3,25 @@ function randomIntBetween(min, max) { // min: 5, max: 10
 }
 
 console.log(randomIntBetween(3, 10));
+
+function productDescription(strings, productName, productPrice) {
+    console.log(strings);
+    console.log(productName);
+    console.log(productPrice);
+    let priceCategory = 'cheap';
+    if(productPrice > 20) {
+        priceCategory = 'fairly priced';
+    }
+    // return 'This is a product';
+    // return `${strings[0]}${productName}${strings[1]}${priceCategory}`
+    return {name: productName, price:productPrice};
+}
+
+
+const prodName = 'Javascript Course';
+const prodPrice = 29.99
+const productOutput = productDescription`
+This product (${prodName}) is ${prodPrice}.
+`;
+
+console.log(productOutput);
