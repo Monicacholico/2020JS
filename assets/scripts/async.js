ccb53679077f413486806552d3d79753d199dd19
Monicacholico <monicachol@hotmail.com>
2020-05-27 23:44:49 -0500

    chapter 355

--- assets/scripts/async.js
+++ assets/scripts/async.js
@@ -1,6 +1,20 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const getPosition = (opts) => {
+ const promise = new Promise ((resolve, reject) => {
+   navigator.geolocation.getCurrentPosition( success => {
+    resolve(success);
+    },error => {
+  
+    }, opts);
+  });
+ return promise;
+  };
+
+
+
+
 const setTimer = (duration) => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -11,16 +25,17 @@ const setTimer = (duration) => {
 };
 
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(
-      postData => {
-        setTimer(2000).then( data => {
-          console.log(data, postData);
-        });
-  }, error => {
-      console.log(error);
+  let positionData;
+  getPosition()
+  .then(postData => {
+    positionData = postData;
+    return setTimer(2000);
+  })
+  .then((data) => {
+    console.log(data, positionData);
   });
-  setTimer(1000).then(() =>{
-    console.log(data, postData);
+  setTimer(1000).then(() => {
+    console.log('Timer done!');
   })
   console.log('Getting position...');
 }
