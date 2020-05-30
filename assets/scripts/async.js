7dd993d52bec48a1336710b695fe5aa5dbb9a363
Monicacholico <monicachol@hotmail.com>
2020-05-29 22:22:29 -0500

    chapter 359

--- assets/scripts/async.js
+++ assets/scripts/async.js
@@ -15,7 +15,7 @@ const getPosition = (opts) => {
 
 
 
-const setTimer = (duration) => {
+const setTimer = duration => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve('Done');
@@ -24,24 +24,33 @@ const setTimer = (duration) => {
   return promise;
 };
 
-function trackUserHandler() {
-  let positionData;
-  getPosition()
-  .then(postData => {
-    positionData = postData;
-    return setTimer(2000);
-  })
-  .catch(err => {
-    console.log(err);
-    return 'on we go ...';
-  })
-  .then((data) => {
-    console.log(data, positionData);
-  });
-  setTimer(1000).then(() => {
-    console.log('Timer done!');
-  })
-  console.log('Getting position...');
+async function trackUserHandler() {
+  // let positionData;
+  let posData;
+  let timerData;
+  try {
+     posData = await getPosition();
+     timerData = await setTimer(2000);
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(timerData, posData);
+
+  // .then(postData => {
+  //   positionData = postData;
+  //   return setTimer(2000);
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  //   return 'on we go ...';
+  // })
+  // .then((data) => {
+  //   console.log(data, positionData);
+  // });
+  // setTimer(1000).then(() => {
+  //   console.log('Timer done!');
+  // })
+  // console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
