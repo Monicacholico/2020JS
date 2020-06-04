bfbd73df11b72f564e48fc5087954a17c00eadde
Monicacholico <monicachol@hotmail.com>
2020-06-03 23:59:34 -0500

    chapter 370

--- assets/scripts/httpRequest.js
+++ assets/scripts/httpRequest.js
@@ -2,4 +2,12 @@ const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
+xhr.responseType = 'json'
+
+xhr.onload = function() {
+    // const listOfPosts = JSON.parse(xhr.response);
+    const listOfPosts = xhr.response;
+    console.log(listOfPosts);
+};
+
 xhr.send();
