05ec2a975ce9c042e272164b4c84d44bf444f93e
Monicacholico <monicachol@hotmail.com>
2020-06-04 22:19:58 -0500

    chapter 371

--- assets/scripts/httpRequest.js
+++ assets/scripts/httpRequest.js
@@ -1,6 +1,10 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
 const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
 
 xhr.responseType = 'json'
 
@@ -8,6 +12,14 @@ xhr.onload = function() {
     // const listOfPosts = JSON.parse(xhr.response);
     const listOfPosts = xhr.response;
     console.log(listOfPosts);
+    for(const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.body
+        listElement.append(postEl);
+    }
 };
 
 xhr.send();
+
+
