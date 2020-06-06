b2713c9fd1ed4e4632e821d739d13024c510b716
Monicacholico <monicachol@hotmail.com>
2020-06-05 22:31:23 -0500

    chapter 371

--- assets/scripts/httpRequest.js
+++ assets/scripts/httpRequest.js
@@ -1,25 +1,34 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-const xhr = new XMLHttpRequest();
+function sendHttpReques(method, url) {
+    const promise = new Promise( (resolve, reject) => {
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+    
+        xhr.responseType = 'json'
+        
+        xhr.onload = function() {
+            // const listOfPosts = JSON.parse(xhr.response);
+            const listOfPosts = xhr.response;
+            console.log(listOfPosts);
+            for(const post of listOfPosts) {
+                const postEl = document.importNode(postTemplate.content, true);
+                postEl.querySelector('h2').textContent = post.title.toUpperCase();
+                postEl.querySelector('p').textContent = post.body
+                listElement.append(postEl);
+            }
+        };
+    })
+
+    
+    xhr.send();
+}
 
 
-xhr.responseType = 'json'
-
-xhr.onload = function() {
-    // const listOfPosts = JSON.parse(xhr.response);
-    const listOfPosts = xhr.response;
-    console.log(listOfPosts);
-    for(const post of listOfPosts) {
-        const postEl = document.importNode(postTemplate.content, true);
-        postEl.querySelector('h2').textContent = post.title.toUpperCase();
-        postEl.querySelector('p').textContent = post.body
-        listElement.append(postEl);
-    }
-};
-
-xhr.send();
-
+function fetchPost() {
+    
+}
 
