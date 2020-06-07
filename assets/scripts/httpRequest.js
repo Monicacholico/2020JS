e1f52615b881c6609da968e94d776508491a2d6c
Monicacholico <monicachol@hotmail.com>
2020-06-06 23:11:35 -0500

    chapter 374:

--- assets/scripts/httpRequest.js
+++ assets/scripts/httpRequest.js
@@ -1,18 +1,32 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
-function sendHttpReques(method, url) {
+function sendHttpReques(method, url, data) {
     const promise = new Promise( (resolve, reject) => {
-
-
+        
         const xhr = new XMLHttpRequest();
+
         xhr.open(method, url);
     
         xhr.responseType = 'json'
         
         xhr.onload = function() {
+            resolve(xhr.response);
             // const listOfPosts = JSON.parse(xhr.response);
-            const listOfPosts = xhr.response;
+ 
+        };
+        xhr.send(JSON.stringify(data));
+    });
+    return promise;
+}
+
+
+async function fetchPost() {
+    const responseData = await sendHttpReques(
+        'GET', 
+        'https://jsonplaceholder.typicode.com/posts'
+        );
+            const listOfPosts = responseData;
             console.log(listOfPosts);
             for(const post of listOfPosts) {
                 const postEl = document.importNode(postTemplate.content, true);
@@ -20,15 +34,18 @@ function sendHttpReques(method, url) {
                 postEl.querySelector('p').textContent = post.body
                 listElement.append(postEl);
             }
-        };
-    })
+ 
+        }
+async function createPost(title, content) {
+    const userId = Math.random();
+    const post = {
+        title: title,
+        body: content,
+        userId: userId
+    };
+    sendHttpReques('POST','https://jsonplaceholder.typicode.com/posts', post);
 
-    
-    xhr.send();
 }
-
-
-function fetchPost() {
-    
-}
+fetchPost();
+createPost('DUMMY', 'A dummy post!');
 
