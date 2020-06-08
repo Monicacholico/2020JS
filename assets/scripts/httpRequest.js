09fc770bc1e14d34ef6d7f6976886cd9845197eb
Monicacholico <monicachol@hotmail.com>
2020-06-07 22:35:32 -0500

    chapter 376

--- assets/scripts/httpRequest.js
+++ assets/scripts/httpRequest.js
@@ -1,5 +1,8 @@
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
 function sendHttpReques(method, url, data) {
     const promise = new Promise( (resolve, reject) => {
@@ -31,7 +34,8 @@ async function fetchPost() {
             for(const post of listOfPosts) {
                 const postEl = document.importNode(postTemplate.content, true);
                 postEl.querySelector('h2').textContent = post.title.toUpperCase();
-                postEl.querySelector('p').textContent = post.body
+                postEl.querySelector('p').textContent = post.body;
+                postEl.querySelector('li').id = post.id;
                 listElement.append(postEl);
             }
  
@@ -46,6 +50,19 @@ async function createPost(title, content) {
     sendHttpReques('POST','https://jsonplaceholder.typicode.com/posts', post);
 
 }
-fetchPost();
-createPost('DUMMY', 'A dummy post!');
+// fetchPost();
+fetchButton.addEventListener('click', fetchPost);
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const enteredTitle = event.currentTarget.querySelector('#title').value;
+    const enteredContent = event.currentTarget.querySelector('#content').value;
+    createPost(enteredTitle, enteredContent);
+})
+
+postList.addEventListener('click', event => {
+    if (event.target.tagName === 'BUTTON') {
+        const postId = event.target.closest('li').id;
+        sendHttpReques('DELETE',`https://jsonplaceholder.typicode.com/posts/${postId}`);
+    }
+})
 
