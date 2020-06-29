8f6deea792ca8654faee6dd80739768223c17f71
Monicacholico <monicachol@hotmail.com>
2020-06-28 22:58:58 -0500

    chapter 428

---
+++ assets/scripts/storage.js
@@ -0,0 +1,30 @@
+const storeBtn = document.getElementById('store-btn');
+const retrBtn = document.getElementById('retrieve-btn');
+
+const userId = 'u123';
+const user = {
+    name: 'Max',
+    age: 30,
+    hobbies: ['sports', 'cooking']
+};
+
+// localStorage.setItem('uid', user);
+
+storeBtn.addEventListener('click', () => {
+    sessionStorage.setItem('uid', userId);
+    localStorage.setItem('user', JSON.stringify(user));
+});
+
+retrBtn.addEventListener('click', () => {
+    const extractedId = sessionStorage.getItem('uid');
+    const extractedUser = JSON.parse(localStorage.getItem('user'));
+    console.log(extractedUser);
+    if (extractedId && extractedUser) {
+        console.log( 'Got the id - ' + extractedId);
+        console.log(extractedUser);
+    } else {
+        console.log('could not find the id');
+    }
+});
+
+
