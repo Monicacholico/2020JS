b0bb8221c860ce351460c455d71b773eb12a1c60
Monicacholico <monicachol@hotmail.com>
2020-07-06 23:37:30 -0500

    chapter 440

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,21 @@ module.exports = {
         path: path.resolve(__dirname, 'assets', 'scripts'),
         publicPath: 'assets/scripts/'
     },
-    devtool:'cheap-module-eval-source-map'
+    devtool:'cheap-module-eval-source-map',
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
     // devServer: {
     //     contentBase: './
     // }
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -13,7 +13,21 @@
 //     devtool:'cheap-source-map'
 //     // devServer: {
 //     //     contentBase: './
-//     // }
+//     // },
+// module: {
+//     rules: [
+//       {
+//         test: /\.m?js$/,
+//         exclude: /(node_modules)/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env']
+//           }
+//         }
+//       }
+//     ]
+//   },
 //     // plugins: [
 //     //     new CleanPlugin.CleanWebpackPlugin()
 //     // ]
