2a2df11af2f2a003b4f1576af66837f46401ce0f
Monicacholico <monicachol@hotmail.com>
2020-07-19 22:55:28 -0500

    chapter 470

--- assets/scripts/meta.js
+++ assets/scripts/meta.js
@@ -7,8 +7,11 @@ const user = {
     // id: 'p1',
     [uid]: 'p1',
     name: 'Max',
-    age: 30
+    age: 30,
+    [Symbol.toStringTag]: 'User'
 };
+
+user[uid] = 'p3';
 
 // app land => Using the library
 
@@ -16,3 +19,40 @@ user.id = 'p2'; //this should not be possible;
 
 console.log(user[Symbol('uid')]);
 console.log(Symbol('uid') === Symbol('uid'));
+console.log(user.toString());
+
+// iterators
+
+const company = {
+    curEmployee: 0,
+    employees: ['Max', 'Manu', 'Anna'],
+    next() {
+        const returnValue = { 
+            value: this.employees[this.curEmployee], 
+            done: false};
+        this.curEmployee++;
+        return returnValue;
+    },
+    [Symbol.iterator]: function* employeeGenerator() {
+        // let employee = company.next();
+
+        // while(!employee.done) {
+        //     yield employee.value;
+        //     employee = company.next();
+        // }
+        const currentEmployee = 0;
+        while (currentEmployee < this.employees.length){
+            yield this.employees[currentEmployee];
+        }
+    }
+};
+
+// let employee = company.next();
+// while(!employee.done) {
+//     console.log(employee.value);
+//     employee = company.next();
+// }
+
+// for(const employee of company) {
+//     console.log(employee);
+// }
