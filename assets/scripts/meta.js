79e3ce56e0898c0cfdc44728e88431fe1a5cc1e5
Monicacholico <monicachol@hotmail.com>
2020-07-20 23:31:37 -0500

    finished chapter 26

--- assets/scripts/meta.js
+++ assets/scripts/meta.js
@@ -24,15 +24,16 @@ console.log(user.toString());
 // iterators
 
 const company = {
-    curEmployee: 0,
+    // curEmployee: 0,
     employees: ['Max', 'Manu', 'Anna'],
-    next() {
-        const returnValue = { 
-            value: this.employees[this.curEmployee], 
-            done: false};
-        this.curEmployee++;
-        return returnValue;
-    },
+    // next() {
+    //     const returnValue = { 
+    //         value: this.employees[this.curEmployee], 
+    //         done: false};
+    //     this.curEmployee++;
+    //     return returnValue;
+    // },
+    // getEmployee: function* employeeGenerator() {
     [Symbol.iterator]: function* employeeGenerator() {
         // let employee = company.next();
 
@@ -40,19 +41,73 @@ const company = {
         //     yield employee.value;
         //     employee = company.next();
         // }
-        const currentEmployee = 0;
+        let currentEmployee = 0;
         while (currentEmployee < this.employees.length){
             yield this.employees[currentEmployee];
+            currentEmployee++;
         }
     }
 };
 
 // let employee = company.next();
+
 // while(!employee.done) {
 //     console.log(employee.value);
 //     employee = company.next();
 // }
 
-// for(const employee of company) {
-//     console.log(employee);
-// }
+for (const employee of company) {
+    console.log(employee);
+}
+
+console.log([...company]);
+
+// const it = company.getEmployee();
+
+// console.log(it.nex());
+// console.log(it.nex());
+// console.log(it.nex());
+// console.log(it.nex());
+// console.log(it.nex());
+
+// Reflect API
+
+const course = {
+    title: 'Javascript - The Complete Guide'
+};
+
+Reflect.setPrototypeOf(course, {toString() {
+    return this.title;
+}
+});
+
+//Reflect.deleteProperty(course, 'title');
+
+// Object.deleteProperty(course, 'title'); It won't work 'cause Object doesn't have this delete feature
+
+// delete course.title;
+
+console.log(course.toString());
+
+// Proxy API
+
+const courseHandler = {
+    get(obj, propertyName) {
+        console.log(propertyName);
+        if(propertyName === 'length') {
+            return 0;
+        }
+        return obj[propertyName] || 'NOT FOUND';
+    },
+    set(obj, propertyName, newValue) {
+        console.log('Sending data ...');
+        if(propertyName === 'rating') {
+            return;
+        }
+        obj[propertyName] = newValue;
+    }
+};
+
+const pCourse = new Proxy(course, courseHandler);
+pCourse.rating = 5;
+console.log(pCourse.title, pCourse.length);
