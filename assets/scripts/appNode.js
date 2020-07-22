269d76b35058f5da9bea9dab872f54235079e264
Monicacholico <monicachol@hotmail.com>
2020-07-21 23:33:02 -0500

    chapter 480

--- assets/scripts/appNode.js
+++ assets/scripts/appNode.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+// const http = require('http');
+
+// const server = http.createServer((request, response) => {
+//     response.setHeader('Content-Type', 'text/html');
+//     response.write('Hello there');
+//     response.end();
+// });
+
+// server.listen(3000);
+const fs = require('fs');
+
+fs.readFile('user-data.txt', (err, data) => {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log(data.toString());
+});
+
+fs.writeFile('user-data.txt', 'username=Max', err => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Wrote to file!');
+    }
+});
--- assets/scripts/nodeapp.js
+++ assets/scripts/nodeapp.js
@@ -5,5 +5,5 @@ const http = require('http');
 const server = http.createServer((request, response) => {
     response.write('Hello There');
     response.end();
-})
+});
 server.listen(3000);
