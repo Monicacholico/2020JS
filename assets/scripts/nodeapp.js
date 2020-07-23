cd1702c0efc8b78f247cd0a6c2f98e364bcb43eb
Monicacholico <monicachol@hotmail.com>
2020-07-22 23:00:40 -0500

    chapter 487

--- assets/scripts/nodeapp.js
+++ assets/scripts/nodeapp.js
@@ -3,7 +3,25 @@ console.log('hello world');
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.write('Hello There');
-    response.end();
+    let body = [];
+    console.log(request.method, request.url);
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    });
+    request.on('end', () => {
+        body = Buffer.concat(body).toString();
+        let userName = 'Unknown user';
+        if (body) {
+            userName = body.split('=')[1];
+            console.log(body);
+        }
+
+        response.setHeader('Conten-Type', 'text/html');
+        response.write(
+            `<h1>Hi ${userName}<form method="POST" action="/"><input name="username" type="text"><button type="submit">Send</button>`
+            );
+        response.end();
+    });
+
 });
 server.listen(3000);
