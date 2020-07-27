b23f7ec9e253b82ff04da2782978fa8bc339330d
Monicacholico <monicachol@hotmail.com>
2020-07-27 00:14:09 -0500

    chapter 493

--- assets/scripts/routes/location.js
+++ assets/scripts/routes/location.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+
 
 const router = express.Router();
+
+const url = 'mongodb+srv://monica:LTPndweuiVhNdSMi@cluster2020js.8dsce.mongodb.net/locations?retryWrites=true&w=majority';
+
+const client = new MongoClient(url);
 
 const locationStorage = {
     locations: []
@@ -8,12 +15,26 @@ const locationStorage = {
 
 router.post('/add-location', (req, res, next) => {
     const id = Math.random();
-    locationStorage.locations.push({
-        id: id, 
-        address: req.body.address, 
-        coords: {lat: req.body.lat, lng: req.body.lng}
-    });
-    res.json({message: 'Store location!', locId: id});
+    client.connect(function(err, client) {
+      
+        const db = client.db(dbName);
+      
+        // Insert a single document
+        db.collection('user-locations').insertOne(
+            {
+                address: req.body.address,
+                coords: { lat: req.body.lat, lng: req.body.lng}
+            }, 
+            function(err, r) {
+                console.log(r);
+                res.json({message: 'Store location!', locId: id});
+        });
+      });
+    // locationStorage.locations.push({
+    //     id: id, 
+    //     address: req.body.address, 
+    //     coords: {lat: req.body.lat, lng: req.body.lng}
+    // });
 });
 
 router.get('/location/:lid', (req, res, next) => {
